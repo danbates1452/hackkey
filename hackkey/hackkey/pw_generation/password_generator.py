bfb3c21e9279e5f7cc614d57a1f8bb7a3dae41bc
Alex Kirk <76045215+ErcModel3@users.noreply.github.com>
2024-02-17 22:04:37 +0000

    Update password_generator.py
    Removed redundant code

--- hackkey/hackkey/pw_generation/password_generator.py
+++ hackkey/hackkey/pw_generation/password_generator.py
@@ -3,14 +3,15 @@ import string
 
 def generate_secure_password(length=12, level=1):
     # to be changed to iinteract with the bootstrap element to create custom length passwords.
-    """Generate a secure password."""
-    alphabet = string.ascii_letters + string.digits + string.punctuation
-    password = ''.join(secrets.choice(alphabet) for i in range(length))
-    return password
+    # """Generate a secure password."""
+    # alphabet = string.ascii_letters + string.digits + string.punctuation
+    # password = ''.join(secrets.choice(alphabet) for i in range(length))
+    # return password
 
     if(level == 1):
         print(|"Nah, I'd win")
         alphabet = string.ascii_letters + string.digits + string.punctuation
         password = ''.join(secrets.choice(alphabet) for i in range(length))
+        return password
     elif(level == 2):
         # code for enhanced code generation will apply
