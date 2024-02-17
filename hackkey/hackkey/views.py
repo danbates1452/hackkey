efbf861428a60266da646423441ae2529171beb8
Daniel Bates <42376377+danbates1452@users.noreply.github.com>
2024-02-17 19:20:39 +0000

    fixed active navbar items

--- hackkey/hackkey/views.py
+++ hackkey/hackkey/views.py
@@ -5,25 +5,25 @@ from django.views.decorators.csrf import csrf_exempt
 @csrf_exempt
 def index(request):
     context = {
-        'message': 'index',
+        'page_name': 'index',
     }
     return render(request, 'index.html', context=context)
 
 def login(request):
-    context = {
-        'message': 'login',
+    context = {+
+        'page_name': 'login',
     }
     
     return render(request, 'login.html', context=context)
 
 def gacha(request):
     context = {
-        'message': 'gacha',
+        'page_name': 'gacha',
     }
     return render(request, 'gacha.html', context=context)
 
 def passwords(request):
     context = {
-        'message': 'passwords',
+        'page_name': 'passwords',
     }
     return render(request, 'passwords.html', context=context)
